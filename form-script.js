052fe01b91975ef9f4e0534939cea8afefbdc6e1
shashibaranwal <148945019+shashibaranwal@users.noreply.github.com>
2024-02-01 17:36:35 +0545

    Create form-script.js

---
+++ form-script.js
@@ -0,0 +1,9 @@
+let signIn = document.getElementById("sign-in");
+let signUp = document.getElementById("sign-up");
+let username = document.getElementById("username");
+let title = document.getElementById("title");
+
+
+signIn onclick = function(){
+    username.style.maxHeignt="0";
+}
