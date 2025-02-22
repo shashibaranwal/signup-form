218a4bd8a4f6c12c352cb3f249523410cbb50ff1
shashibaranwal <shashibaranwal09@gmail.com>
2025-02-23 00:17:27 +0545

    verified login set

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-document.getElementById("loginForm").addEventListener('submit', function(e) {
+document.getElementById("loginForm").addEventListener('submit', function (e) {
   e.preventDefault();
 
   const email = document.getElementById("userEmail").value;
@@ -10,17 +10,24 @@ document.getElementById("loginForm").addEventListener('submit', function(e) {
     return;
   }
 
-  const storedUser = JSON.parse(localStorage.getItem('user'));
-
-  if(!storedUser){
-    alert("No user found!!");
-  }
-
-  if(storedUser.email === email && storedUser.password === password){
-    alert("Login successful!!");
-    window.location.href = "index.html";
-  } else {
-    alert("Invalid user email or password. Please try again.");
+  try {
+    let storedUser = new Array();
+    storedUser = JSON.parse(localStorage.getItem('users')) ? JSON.parse(localStorage.getItem('users')) : [];
+    if (storedUser.some((v) => {
+      return v.email == email && v.password == password;
+    })) {
+      alert("Login Successful!");
+      let currentUser = storedUser.filter((v) => {
+        return v.email == email && v.password == password
+      })[0];
+      localStorage.getItem('users', currentUser);
+      window.location.href = "index.html";
+    } else {
+      alert("Invalid user email or password. Please try again.");
+    }
+  } catch (error) {
+    console.error("Error saving data:", error);
+    alert("An error occurred while signing up.");
   }
 
 });
